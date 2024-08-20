d469c98f423c62b95a958aeaf9f5bc283c2b57ed
TranPhong9324 <tranphongok1412@gmail.com>
2024-08-20 12:18:59 +0700

    BTTKFrom

---
+++ BaiTapThietKeFrom/frmBai2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTapThietKeFrom
+{
+    public partial class frmBai2 : Form
+    {
+        public frmBai2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChonHang_Click(object sender, EventArgs e)
+        {
+            var item = listBox1.SelectedItem;
+            listBox2.Items.Add(item);
+        }
+
+        private void btnBoHang_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Remove(listBox2.SelectedItem);
+        }
+
+        private void btnTinhTien_Click(object sender, EventArgs e)
+        {
+            int soTien = 0;
+            
+            //Duyệt danh sách mặt hàng ListBox2
+            // Cộng dồn số tiên cần thanh toán
+            foreach (string hang in listBox2.Items)
+            {
+                switch (hang)
+                {
+                    case "Chuột":
+                        soTien += 100000;
+                        break;
+                    case "Bàn phím":
+                        soTien += 150000;
+                        break;
+                    case "Máy in":
+                        soTien += 200000;
+                        break;
+                    case "USB King":
+                        soTien += 200000;
+                        break;
+                    default:
+                        break;
+                }
+                lblSoTien.Text = soTien + "đồng";    
+            }
+        }
+    }
+}
